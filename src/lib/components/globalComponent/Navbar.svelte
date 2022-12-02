74c9c3247e0913b32164d64f61ea3e7693a401b4
nishantmohilnhn <nniisshhaanntt661199@gmail.com>
2022-12-02 14:58:55 +0530

    nav bar 95 done design changes for mobile not

--- src/lib/components/globalComponent/Navbar.svelte
+++ src/lib/components/globalComponent/Navbar.svelte
@@ -1,3 +1,4 @@
+<div class=" sticky top-0 w-full bg-white backdrop-filter backdrop-blur-lg">
 <div class="items-center justify-between sm:flex lg:justify-around">
     <div class="flex items-center justify-between">
       <a class="m-1 flex shrink-0 items-center gap-1 p-1" href="/">
@@ -6,19 +7,25 @@
         </svg>
         <h1 class="text-xl font-bold">DiaperExperts</h1>
       </a>
-      <div class="sm:hidden">
+      <button on:click={bawa} class="sm:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-5 h-9 w-9">
-          <path class="block" stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
-          <path class="hidden" stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
         </svg>
-      </div>
+      </button>
     </div>
-    <div class="items-center sm:flex sm:gap-4 sm:pr-5">
-      <a class="block pr-5 text-right text-base sm:pr-0" href="/">Home</a>
-      <a class="block pr-5 text-right text-base sm:pr-0" href="/services">Services</a>
-      <a class="block pr-5 text-right text-base sm:pr-0" href="/cta">Work With Us</a>
-      <a class="block pr-5 text-right text-base sm:pr-0" href="/about">About</a>
-      <a class="block pr-5 text-right text-base sm:pr-0" href="/Getintouch">Get in Touch</a>
+    <div class:hidden class="items-center sm:flex sm:gap-4 sm:pr-5">
+      <a on:click={bawa}  class="block pr-6 text-right  text-base sm:pr-0" href="/">Home</a>
+      <a on:click={bawa}  class="block pr-6 text-right  text-base sm:pr-0" href="/services">Services</a>
+      <a on:click={bawa}  class="block pr-6 text-right  text-base sm:pr-0" href="/cta">Work With Us</a>
+      <a on:click={bawa}  class="block pr-6 text-right  text-base sm:pr-0" href="/about">About</a>
+      <a on:click={bawa}  class="block pr-6 text-right  text-base sm:pr-0 mb-2 sm:mb-0" href="/Getintouch">Get in Touch</a>
     </div>
   </div>
-  
+  </div>
+
+  <script>
+    let hidden = false;
+    function bawa() {
+      hidden = !hidden;
+    }
+  </script>
