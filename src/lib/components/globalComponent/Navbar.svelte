30ea0d1d9e010be669dd413d24aab351c79f2f06
nishantmohilnhn <nniisshhaanntt661199@gmail.com>
2022-12-02 13:56:02 +0530

    nav + no nav js

---
+++ src/lib/components/globalComponent/Navbar.svelte
@@ -0,0 +1,24 @@
+<div class="items-center justify-between sm:flex lg:justify-around">
+    <div class="flex items-center justify-between">
+      <a class="m-1 flex shrink-0 items-center gap-1 p-1" href="/">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="h-10 w-10 rounded-full bg-teal-600 p-2 text-white" viewBox="0 0 24 24">
+          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
+        </svg>
+        <h1 class="text-xl font-bold">DiaperExperts</h1>
+      </a>
+      <div class="sm:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-5 h-9 w-9">
+          <path class="block" stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+          <path class="hidden" stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+      </div>
+    </div>
+    <div class="items-center sm:flex sm:gap-4 sm:pr-5">
+      <a class="block pr-5 text-right text-base sm:pr-0" href="/">Home</a>
+      <a class="block pr-5 text-right text-base sm:pr-0" href="/services">Services</a>
+      <a class="block pr-5 text-right text-base sm:pr-0" href="/cta">Work With Us</a>
+      <a class="block pr-5 text-right text-base sm:pr-0" href="/about">About</a>
+      <a class="block pr-5 text-right text-base sm:pr-0" href="/Getintouch">Get in Touch</a>
+    </div>
+  </div>
+  
