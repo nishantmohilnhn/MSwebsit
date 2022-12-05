881617d03ba12fcdfae7b91e61547486d7c32f96
nishantmohilnhn <nniisshhaanntt661199@gmail.com>
2022-12-05 16:21:04 +0530

    sdf

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<div class=" flex items-center justify-center m-4 text-3xl sm:text-6xl">
+    <h1 class=" text-my-nvy1 "> {$page.status}:
+         page yo are looking for is not available
+    </h1>
+    
+</div>
