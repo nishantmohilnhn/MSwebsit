80581281c9ab9674f52ff851f95b6c0863640f2d
nishantmohilnhn <nniisshhaanntt661199@gmail.com>
2022-12-06 14:19:04 +0530

    nev

--- src/lib/components/ActionButton.svelte
+++ src/lib/components/ActionButton.svelte
@@ -1,12 +1,12 @@
 <div class="fixed bottom-0 flex w-full items-center justify-center gap-2 bg-neutral-100 p-1 sm:hidden">
-    <a class="from-my-nvy1 my-1 ml-2 flex w-1/2 items-center justify-center gap-2 rounded-full bg-gradient-to-r to-pink-800 p-3 text-center text-xl text-white" href="/contact">
+    <a class="from-my-nvy1 my-1 ml-2 flex w-1/2 items-center justify-center gap-2 rounded-full bg-gradient-to-r to-pink-800 p-1 text-center text-xl text-white" href="/contact">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
       </svg>
   
       Contact</a
     >
-    <a class="from-my-nvy1 my-1 mr-2 flex w-1/2 items-center justify-center gap-2 rounded-full bg-gradient-to-r to-pink-800 p-3 text-center text-xl text-white" href="https://api.whatsapp.com/send?phone=918219491416">
+    <a class="from-my-nvy1 my-1 mr-2 flex w-1/2 items-center justify-center gap-2 rounded-full bg-gradient-to-r to-pink-800 p-1 text-center text-xl text-white" href="https://api.whatsapp.com/send?phone=918219491416">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 175.216 175.552" class="h-8 w-8">
         <defs>
           <linearGradient id="b" x1="85.915" x2="86.535" y1="32.567" y2="137.092" gradientUnits="userSpaceOnUse">
